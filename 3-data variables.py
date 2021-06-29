b69e9a9bc07b8cd942d449526721a15d45100e3f
Nyajur <s.emunga@gmail.com>
2021-06-29 10:46:26 -0400

    intro to variables and coding sequence

---
+++ 3-data variables.py
@@ -0,0 +1,7 @@
+character_name = "simon"
+character_age = "35"
+print(character_name  +  " seems "  +  character_age +  " but is he the rober?")
+print(character_name  +  " seems "  +  character_age +  " smells like pop")
+character_name = "Mike"
+print(character_name  +  " seems "  +  character_age +  " has a longer beard?")
+print(character_name  +  " seems "  +  character_age +  " is not as analytical as he should be")
