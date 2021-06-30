1862801481c7342f921bcd38920592296e2fd90e
Nyajur <s.emunga@gmail.com>
2021-06-30 08:50:21 -0400

    input

---
+++ 6-user_input.py
@@ -0,0 +1,6 @@
+name = input("enter your name: " )
+print (name)
+print ((name) + " whats the best pizza place around here?")
+age = input(" Enter your age: ")
+print ((name) + " whats the best pizza place around here?")
+print ("At age " + (age) + " you need little more time to know what your are doing, Ok "+ (name))
