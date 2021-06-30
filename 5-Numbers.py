64a5d903248dd7de6226efe47d4ef9ce38629513
Nyajur <s.emunga@gmail.com>
2021-06-30 08:18:33 -0400

    numbers

---
+++ 5-Numbers.py
@@ -0,0 +1,14 @@
+from math import *
+print(2)
+print(10 % 4)
+my_num = -6
+print(str(my_num) + " my most boring number")
+print(abs(my_num))
+print(pow(5, 2))
+print(min(5, 2))
+print(max(5, 2))
+print(round(5.2))
+print(round(5.7))
+print(floor(256.57))
+print(ceil(58.7))
+print(sqrt(90.7))
